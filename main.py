5244106227a70942b5ee26e40fec791b15bcbd68
Kerem Cömert <kerem0comert@gmail.com>
2022-08-15 02:10:35 +0200

    Initialize with random seed

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import numpy as np
 import os
 from time import sleep
 
+
 board = list()
-for _ in range(30):
-    board.append(np.zeros(100))
+SEED_ALIVE_PROBABILITY = 0.08
+BOARD_X = 100
+BOARD_Y = 30
+EPOCHS = 1000
 
 def print_board():
     print("|", end="")
@@ -14,46 +17,59 @@ def print_board():
     for row in board:
         print("|", end="")
         for column in row:
-            if column: print("█", end="")
-            else: print(" ", end="")
+            if column:
+                print("█", end="")
+            else:
+                print(" ", end="")
         print("|")
     for _ in board:
         print("-", end="")
 
-def get_neighbors_count(i: int, j: int) -> int: 
+
+def get_neighbors_count(i: int, j: int) -> int:
     neighbors_count = 0
-    for x in range(i-1, i+2):
-        for y in range(j-1, j+2):
+    for x in range(i - 1, i + 2):
+        for y in range(j - 1, j + 2):
             try:
-                if board[x][y]: neighbors_count += 1
+                if board[x][y]:
+                    neighbors_count += 1
             except ValueError:
                 continue
             except IndexError:
                 continue
     return neighbors_count
 
+
 def advance():
     for i in range(len(board)):
         for j in range(len(board[i])):
-            try: 
-                neighbors_count = get_neighbors_count(i,j)
+            try:
+                neighbors_count = get_neighbors_count(i, j)
                 if board[i][j]:
                     if neighbors_count < 2 or neighbors_count > 3:
                         board[i][j] = 0
                 else:
                     if neighbors_count == 3:
-                        board[i][j] = 1       
+                        board[i][j] = 1
             except ValueError:
                 continue
-    os.system('cls')
+    os.system("cls")
     print_board()
 
+
 def main():
-   for i in range(10):
-       board[i][10] = 1    
-   for i in range(1000):
+    for _ in range(BOARD_Y):
+        board.append(
+            np.random.choice(
+                [0, 1],
+                size=BOARD_X,
+                p=[1 - SEED_ALIVE_PROBABILITY, SEED_ALIVE_PROBABILITY],
+            )
+        )
+    for _ in range(EPOCHS):
         advance()
         sleep(0.2)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
